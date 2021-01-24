243cf697d8fdf4c00f189a9e3bf540b5eb10afd9
Jalal <mjalal_nse4@scme.nust.edu.pk>
2021-01-24 23:19:26 +0500

    added about page

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name= 'polls/home.html'
+
+class AboutPageView(TemplateView):
+    template_name= 'polls/about.html'
